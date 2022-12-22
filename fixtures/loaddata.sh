69ef7e2ebdd6b247e32f6569956ec62757ba4fa2
Thành Phan <94797743+Thanhpt21@users.noreply.github.com>
2022-12-22 23:24:24 +0700

    first commit

---
+++ fixtures/loaddata.sh
@@ -0,0 +1,6 @@
+python manage.py loaddata fixtures/users.json --app accounts.account
+python manage.py loaddata fixtures/userprofiles.json --app accounts.userprofile
+python manage.py loaddata fixtures/categories.json --app category.category
+python manage.py loaddata fixtures/products.json --app store.product
+python manage.py loaddata fixtures/productgalleries.json --app store.productgallery
+python manage.py loaddata fixtures/variants.json --app store.variation
